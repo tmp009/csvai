f82e510a4ada38c5aede9dc9e72b52f54a3d8025
tmp009 <128413103+tmp009@users.noreply.github.com>
2024-02-06 14:06:39 +0200

    support query with thread

--- app/index.js
+++ app/index.js
@@ -8,6 +8,17 @@ const fs = require('fs')
 const { OpenAI } = require('openai');
 const openai = new OpenAI();
 
+const assistantPath = './assistant.json'
+const { createAssistant, runAssistant } = require('./lib/assistant');
+
+if (!fs.existsSync(assistantPath)) {
+  createAssistant('CSVAI', 'You are a assistant that handles CSV data. Answer questions related to CSV data.')
+    .then(assistant => fs.writeFileSync(assistantPath, JSON.stringify({ id: assistant.id })))
+    .catch(console.log)
+}
+
+const assistantId = require(assistantPath).id
+
 async function perform(message, csv) {
   const messages = [
     {
@@ -54,5 +65,60 @@ app.post('/query', async (req, res) => {
     return res.json(answer.data);
   }
 });
+
+app.post('/thread/new', async (req, res) => {
+  const csv = req.body.csv;
+
+  if (!csv) {
+    return res.status(400).json({ error: "Missing required parameter: csv" })
+  }
+
+  const thread = await openai.beta.threads.create({
+    messages: [
+      {
+        role: 'user', content: 'Always return the data as a JSON array in a object with the key "data". If the question or task is vague or not a query for the CSV then return the reason as key "error" and always keep key "data" as an empty array.',
+      },
+      {
+        role: 'user', content: csv
+      }
+    ]
+  });
+
+  return res.json({threadId: thread.id.replace('thread_', '')})
+});
+
+app.post('/thread/:threadId/query', async (req, res) => {
+  const threadId = 'thread_' + req.params.threadId;
+  const prompt = req.body.prompt;
+
+  if (!prompt) {
+    return res.status(400).json({ error: "Missing required parameter: prompt" })
+  }
+
+  try {
+    await openai.beta.threads.messages.create(threadId, {
+        role:'user',
+        content:prompt
+    })
+      
+    const resp = await runAssistant(assistantId, threadId);
+  
+    const cleanedData = resp.content[0].text.value.replace(/^[\s\S]*?```json/, '').replace(/\s*```$/, '')
+    
+    return res.json(JSON.parse(cleanedData))
+
+  } catch (error) {
+    return res.status(error.status).json({ error: `Failed to query thread: ${error.message}`})
+  }
+
+})
+
+app.delete('/thread/:threadId/', async (req, res) => {
+  const threadId = 'thread_' + req.params.threadId;
+
+  await openai.beta.threads.del(threadId) 
+    .then(()=> res.json({status: 200}))
+    .catch(error => { res.status(error.status).json({ error: `Failed to delete thread: ${error.message}`}) })
+})
 
 app.listen(3000, '0.0.0.0', () => console.log('http://0.0.0.0:3000/'))
